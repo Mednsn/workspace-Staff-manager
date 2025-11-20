3c8eb78fe234f98ebddf29549ff6ea44299e634a
mohamed <medolgholb@gmail.com>
2025-11-20 11:48:55 +0100

    modifier des erreurs dans function regex

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 document.getElementById("annuler-formulaire").addEventListener("click", resetForme);
-function resetForme(){
+function resetForme() {
     document.forms["ajouter"].reset();
     let intp = document.querySelectorAll('input');
     intp.forEach(inpt => {
@@ -19,17 +19,17 @@ urlpicture.addEventListener("input", function () {
     photos.classList.add(`bg-[url(${urlpicture.value})]`);
 })
 
-let btnAjouterEX=document.getElementById("ajouter-exp");
-btnAjouterEX.addEventListener("click",function(){
-    let spaceEX= document.getElementById("pluus-experiences");
-    spaceEX.innerHTML+=`<div class="grid experience-count gap-2 bg-white border p-2 rounded-md mt-3">
+let btnAjouterEX = document.getElementById("ajouter-exp");
+btnAjouterEX.addEventListener("click", function () {
+    let spaceEX = document.getElementById("pluus-experiences");
+    spaceEX.innerHTML += `<div class="grid experience-count gap-2 bg-white border p-2 rounded-md mt-3">
                                 <div class="grid ">
                                     <label for="">nom :</label>
-                                    <input type="text" id="nameexper" class="bg-gray-200 h-7 border rounded-sm ">
+                                    <input type="text" name="nameexper" class="bg-gray-200 h-7 border rounded-sm ">
                                 </div>
                                 <div class="grid ">
                                     <label for="">role:</label>
-                                    <input type="text" id="roles" class="bg-gray-200 h-7 border rounded-sm ">
+                                    <input type="text" name="roles" class="bg-gray-200 h-7 border rounded-sm ">
                                 </div>
                             </div>`;
 })
@@ -39,9 +39,10 @@ btnAjouterEX.addEventListener("click",function(){
 document.forms["ajouter"].addEventListener("submit", validationForm);
 
 function validationForm(e) {
-    
+
 
     e.preventDefault();
+   
     let nom = document.getElementById("nomworker");
     let role = document.getElementById("roleworker");
     let emil = document.getElementById("emailworker");
@@ -57,8 +58,8 @@ function validationForm(e) {
         if (!urlregex.test(urlpicture.value)) {
             urlpicture.style.borderColor = "red";
             return;
-        } else {urlpicture.style.borderColor = "green";}
-    } 
+        } else { urlpicture.style.borderColor = "green"; }
+    }
     let nameregex = /^[a-zA-Z\s?]+$/;
     if (nom.value == "") {
         nom.style.borderColor = "red";
@@ -69,7 +70,8 @@ function validationForm(e) {
             return;
         } else {
             nom.style.borderColor = "green";
-    }};
+        }
+    };
 
     if (role.value == "") {
         role.style.borderColor = "red";
@@ -80,7 +82,8 @@ function validationForm(e) {
             return;
         } else {
             role.style.borderColor = "green";
-        } };
+        }
+    };
     let emilregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emil.value == "") {
         emil.style.borderColor = "red";
@@ -91,7 +94,8 @@ function validationForm(e) {
             return;
         } else {
             emil.style.borderColor = "green";
-     }};
+        }
+    };
     let phoneregex = /^\d+$/;
     if (telephon.value == "") {
         telephon.style.borderColor = "red";
@@ -102,7 +106,9 @@ function validationForm(e) {
             return;
         } else {
             telephon.style.borderColor = "green";
-    }};
+        }
+    };
+   
     let staff = {
         urlphotos: urlpicture.value,
         firstname: nom.value,
@@ -110,7 +116,7 @@ function validationForm(e) {
         emilstaff: emil.value,
         telephone: telephon.value,
         experiencetab: []
-        }
+    }
     expercount.forEach(exper => {
         let nameexp = document.getElementById("nameexper");
         let rolexp = document.getElementById("roles");
@@ -136,67 +142,79 @@ function validationForm(e) {
                 return;
             } else { rolexp.style.borderColor = "green"; }
         };
-         
     
-    
-            let expobj={
-                nameExp: exper.nameexp,
-                roleExp: exper.rolexp
-            };
-            staff.experiencetab(expobj)
+        let expobj = {
+            nameExp: nameexp.value,
+            roleExp: rolexp.value
+        };
+        
+        staff.experiencetab.push(expobj)
     })
-   
-     saveStaffList(staff);
-   resetForme();
+
+    saveStaffList(staff);
+    resetForme();
 }
 
 
-let listUnassigned=[];
-function saveStaffList(staff){
-   
-     listUnassigned.push(staff);
-     
-     setLocatleStorege(listUnassigned);
-     
+function saveStaffList(staff) {
+
+    let listUnassigned = JSON.parse(localStorage.getItem('infolistunassigned'));
+    if (listUnassigned == null) {
+        listUnassigned = []
+    }
+    let count = 0;
+    count++;
+    console.log(count);
+
+
+    listUnassigned.push(staff);
+
+    setLocatleStorege(listUnassigned);
+
 }
 
 
 
 function setLocatleStorege(listUnassigned) {
-   
-    localStorage.setItem('infolistunassigned',JSON.stringify(listUnassigned));
-    
-}
-checkLocaleStorege();
 
-
-function checkLocaleStorege(){  
-    let listUnassigneds=JSON.parse(localStorage.getItem('infolistunassigned'));
-    
-       affichageunassigned(listUnassigneds);
+    localStorage.setItem('infolistunassigned', JSON.stringify(listUnassigned));
+    checkLocaleStorege();
 
 }
 
 
+function checkLocaleStorege() {
+    let listUnassigneds = JSON.parse(localStorage.getItem('infolistunassigned'));
 
- function affichageunassigned(membres){
-    console.log(membres);
+    affichageunassigned(listUnassigneds);
 
-    let cardUnassigned=document.getElementById("card-staff-unassigned");
-   membres.forEach((elemt,index)=>{
-     cardUnassigned.innerHTML+=`<button onclick="afficheProfile(${index})" command="show-modal" commandfor="dialog-profile">
-                        <div id="info-profile" class="flex gap-2 border-2 rounded-xl border-gray-300 p-1">
+}
+
+
+
+function affichageunassigned(membres) {
+
+    let cardUnassigned = document.getElementById("card-staff-unassigned");
+    cardUnassigned.innerHTML = ""
+    membres.forEach((elemt, index) => {
+        cardUnassigned.innerHTML += ` <div id="info-profile" class="flex justify-between border-2 rounded-xl border-gray-300 p-1">
+                             <button onclick="afficheProfile${index})" command="show-modal" commandfor="dialog-profile">
+                            <div class="flex">
                             <div id="imag-staff-unassined" class="rounded-[50%] bg-[url(${elemt.urlphotos})] bg-cover h-14 w-14"></div>
                             <div class="grid">
                                 <strong>${elemt.firstname}</strong>
                                 <small>${elemt.role}</small>
                             </div>
-                        </div>
-                    </button>`
-  } )
- }
+                            </button>
+                            <button><i class="far fa-edit fa-lg"></i></button>
+                            </div>
+                            
+                        </div>`
+    })
+}
 
 //  function afficheProfile(index){
-    
-    
+
+
 //     }
+checkLocaleStorege()
